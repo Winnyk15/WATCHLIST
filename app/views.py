4afb1564986ae880d2d32ce9feb73aa6cdad3d74
Winnyk15 <kahendahwinnie@gmail.com>
2020-09-08 20:01:42 +0300

    display movies to template

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 # Views
 @app.route('/')
@@ -10,6 +11,14 @@ def index():
     '''
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title)
+
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title,popular = popular_movies)
+
+    
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
